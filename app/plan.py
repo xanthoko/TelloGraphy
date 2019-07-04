d024414a2d29fdd89eae039cc73569fc7f6c0c20
xantho <konxantho@gmail.com>
2019-07-04 14:13:09 +0300

    static server for serving the base template

--- app/plan.py
+++ app/plan.py
@@ -3,36 +3,73 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 PORT_NUMBER = 8080
 
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = '/'.join(FILE_DIR.split('/')[:-1])
 
-# This class will handles any incoming request from
-# the browser
+HEADER_MAP = {
+    'png': 'image/png',
+    'jpg': 'image/jpg',
+    'js': 'text/javascript',
+    'css': 'text/css',
+    '': 'text/html'
+}
+
+
 class myHandler(BaseHTTPRequestHandler):
 
-    # Handler for the GET requests
     def do_GET(self):
-        # get the directory of the root of the project
-        file_dir = os.path.dirname(os.path.abspath(__file__))
-        root_dir = '/'.join(file_dir.split('/')[:-1])
-
+        """Handles the GET requests."""
+        resource = self.path
         # read the html to be displayed
-        with open(root_dir + '/templates/base.html') as f:
+        with open(BASE_DIR + '/templates/base.html') as f:
             self.html_tempalte = f.read()
 
+        content, content_type = self.handle_request(self.path)
+
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', content_type)
         self.end_headers()
-        # Send the html message
-        self.wfile.write(self.html_tempalte.encode())
+        self.wfile.write(content)
+
+    def handle_request(self, path):
+        """Returns the content type and the content according to the given path."""
+        # extention is blank if no resource is requested
+        extention = self.path.split('/')[-1].split('.')[-1]
+
+        if extention in ["jpg", "png"]:
+            # read images as binary files
+            with open(BASE_DIR + path, 'rb') as f:
+                content = f.read()
+        elif extention in ['css', 'js']:
+            # encode strings
+            with open(BASE_DIR + path, 'r') as f:
+                content = f.read().encode('utf-8')
+        else:
+            with open(BASE_DIR + '/templates/base.html') as f:
+                content = f.read().encode('utf-8')
+
+        try:
+            # content type according to the extention
+            content_type = HEADER_MAP[extention]
+        except KeyError:
+            content_type = 'text/plain'
+
+        return content, content_type
 
 
-try:
-    # Create a web server and define the handler to manage the
-    # incoming request
-    server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print('Started httpserver on port ', PORT_NUMBER)
+def run():
+    try:
+        # Create a web server and define the handler to manage the
+        # incoming request
+        server = HTTPServer(('', PORT_NUMBER), myHandler)
+        print('Started httpserver on port ', PORT_NUMBER)
 
-    # Wait forever for incoming htto requests
-    server.serve_forever()
+        # Wait forever for incoming htto requests
+        server.serve_forever()
 
-except KeyboardInterrupt:
-    server.socket.close()
+    except KeyboardInterrupt:
+        server.socket.close()
+
+
+if __name__ == '__main__':
+    run()
