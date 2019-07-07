d0c29cf610d5c6fa22be1873777a919980e896e3
xanthoko <konxantho@gmail.com>
2019-07-07 18:21:19 +0300

    keep movements in array

--- static/js/draw.js
+++ static/js/draw.js
@@ -1,27 +1,33 @@
 class Drawer {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
+        this.canvas = document.getElementById("myCanvas");
+        this.ctx = this.canvas.getContext("2d");
+
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height / 2;
+        this.steps = [];
     }
 
     draw(direction) {
         var new_x = 0;
         var new_y = 0;
+
+        // according to the given direction set the change of the position
         switch (direction) {
             case "forward":
                 new_x = 0;
-                new_y = -100;
+                new_y = -50;
                 break;
             case "back":
                 new_x = 0;
-                new_y = 100;
+                new_y = 50;
                 break;
             case "left":
-                new_x = -100;
+                new_x = -50;
                 new_y = 0;
                 break;
             case "right":
-                new_x = 100;
+                new_x = 50;
                 new_y = 0;
                 break;
         }
@@ -29,21 +35,27 @@ class Drawer {
         new_x = this.x + new_x;
         new_y = this.y + new_y;
 
-        var canvas = document.getElementById("myCanvas");
-        var ctx = canvas.getContext("2d");
+        // draw line
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.x, this.y);
+        this.ctx.lineTo(new_x, new_y);
+        this.ctx.stroke();
 
-        ctx.beginPath();
-        ctx.moveTo(this.x, this.y);
-        ctx.lineTo(new_x, new_y);
-        ctx.stroke();
-
+        // update the object's position
         this.x = new_x;
         this.y = new_y;
+
+        this.steps.push(direction);
+        console.log(this.steps);
+    }
+
+    clear() {
+        // clear the drawn canvas
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        // clear the steps array
+        this.steps = [];
     }
 }
 
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-
-const drawer = new Drawer(canvas.width / 2, canvas.height / 2);
+const drawer = new Drawer();
